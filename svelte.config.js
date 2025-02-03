dfd62cbdab818552ab179ebf9240834c90038dca
SylvanFrankin <sylvanfranklin@icloud.com>
2025-02-03 10:41:04 -0500

    fix: build info

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
     kit: {
+        appDir: 'app',
         paths: {
-            base: ""
+            base: dev ? '' : process.env.BASE_PATH,
         },
         adapter: adapter({
             pages: 'build',
