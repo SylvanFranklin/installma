f6ba1b094b56907882fc96f7d5c6ff1061c6a1b0
SylvanFranklin <sylvanfranklin@icloud.com>
2025-02-06 07:29:59 -0500

    feat: options for pms

--- src/lib/index.js
+++ src/lib/index.js
@@ -35,10 +35,15 @@ export function pm_link(pm) {
     }
 }
 
-
+export function parse_options(options) {
+    if (options.length < 30) {
+        return options
+    } else {
+        return "\n\t\t" + options.replaceAll(", ", ",\n\t\t") + "\n\t"
+    }
+}
 
 function clean_names(repo) {
-
     const full = repo
         .replace("https://github.com/", "")
         .replace("github.com/", "")
@@ -53,43 +58,41 @@ function clean_names(repo) {
 
 
 export const templates = {
-    lazy: (url, branch, tag) => {
+    lazy: (url, branch, tag, options, lazy) => {
         const [full, part] = clean_names(url)
         return `{
-    "${full}", ${branch && `\n\tbranch = "${branch}",`}${tag && `\n\ttag = "${tag}",`}
-    opts = {}
-}`
+    "${full}", ${branch && `\n\tbranch = "${branch}",`}${lazy ? `\n\tlazy = true,` : ''}${tag && `\n\ttag = "${tag}",`}${options.length > 0 ? `\n\topts = {${options}}` : ""}\n}`
     },
 
-    packer: (url, branch, tag) => {
+    packer: (url, branch, tag, options, lazy) => {
         const [full, part] = clean_names(url)
         return `{
-    "${full}", ${branch && `\n\tbranch = "${branch}",`}${tag && `\n\ttag = "${tag}",`}
+    "${full}", ${branch && `\n\tbranch = "${branch}",`}${lazy ? `\n\topt = true,` : ''}${tag && `\n\ttag = "${tag}",`}
     config = function()
-        require("${part}").setup() 
+        require("${part}").setup({${options}}) 
     end
 }`
     },
 
 
-    paq: (url, branch) => {
+    paq: (url, branch, tag, options, lazy) => {
         const [full, part] = clean_names(url)
         return `{
-    "${full}", ${branch && `\n\tbranch = "${branch}",`}
-}`
+    "${full}", ${branch && `\n\tbranch = "${branch}",`},
+    ${options.length > 0 ? `opt = {${options}}` : ""}\n}`
     },
 
-    plug: (url, branch, tag) => {
+    plug: (url, branch, tag, options, lazy) => {
         const [full, part] = clean_names(url);
-        return `"${full}" ${branch ? `, { 'branch': '${branch}' }` : ""}${tag ? `, { 'tag': '${tag}' }` : ""}`;
+        return `Plug("${full}"${branch ? `, { 'branch': '${branch}' }` : ""}${tag ? `, { 'tag': '${tag}'}` : ""}${options.length > 0 ? "[ do ] = function()\n end" : ""})`;
     },
 
-    vundle: (url, branch, tag) => {
+    vundle: (url, branch, tag, options, lazy) => {
         const [full] = clean_names(url);
         return `Plugin '${full}'${branch ? `, { 'branch': '${branch}' }` : ""}${tag ? `, { 'tag': '${tag}' }` : ""}`;
     },
 
-    pckr: (url, branch, tag) => {
+    pckr: (url, branch, tag, options, lazy) => {
         const [full, part] = clean_names(url);
         return `{
     "${full}", ${branch ? `\n\tbranch = "${branch}",` : ""}${tag ? `\n\ttag = "${tag}",` : ""}
@@ -98,7 +101,7 @@ export const templates = {
     end
 }`;
     },
-    rockstoml: (url, branch, tag) => {
+    rockstoml: (url, branch, tag, options, lazy) => {
         const [full, part] = clean_names(url);
         return `[plugins.${full}]
 version = "1.0.0"
@@ -106,12 +109,12 @@ opt = true
 `;
 
     },
-    rockscommand: (url, branch, tag) => {
+    rockscommand: (url, branch, tag, options, lazy) => {
         const [full, part] = clean_names(url);
         return `Rocks install ${part}`;
 
     },
-    mini: (url, branch, tag) => {
+    mini: (url, branch, tag, options, lazy) => {
         const [full, part] = clean_names(url);
         return `add({
     source = '${full}',
